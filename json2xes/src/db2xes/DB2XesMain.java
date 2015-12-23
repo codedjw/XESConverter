898ed444eb58e162fd92fb02b0f39f7555142be2
Jiawei Du <jiaweidu.js@gmail.com>
2015-12-23 23:09:13 +0800

    add genXES_AllModule() in DB2XesMain; change filepath (add subdir).

--- json2xes/src/db2xes/DB2XesMain.java
+++ json2xes/src/db2xes/DB2XesMain.java
@@ -20,10 +20,11 @@ public class DB2XesMain {
 		String filepath = "/Users/dujiawei/Desktop/流程挖掘案例/趣医网/趣医网-第三阶段/XES";
 		
 		try {
-//			DB2XesMain.genXES_SpecialCase(filepath, "866102022218694");
-//			DB2XesMain.genXES_SpecialCase(filepath, "867620026805215");
-			DB2XesMain.genXES_All(filepath, true);
-			DB2XesMain.genXES_All(filepath, false);
+//			DB2XesMain.genXES_SpecialCase(filepath+"/cases", "866102022218694");
+//			DB2XesMain.genXES_SpecialCase(filepath+"/cases", "867620026805215");
+//			DB2XesMain.genXES_AllHospital(filepath+"/hospitals", true);
+//			DB2XesMain.genXES_AllHospital(filepath+"/hospitals", false);
+			DB2XesMain.genXES_AllModule(filepath+"/modules");
 		} catch (InstantiationException | IllegalAccessException
 				| ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
@@ -59,7 +60,7 @@ public class DB2XesMain {
 	}
 	
 	// 生成各医院的XES （isDaoyi = true表示导医用户，否则表示一般用户）
-	public static void genXES_All(String filepath, boolean isDaoyi) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+	public static void genXES_AllHospital(String filepath, boolean isDaoyi) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
 		Map<String, Integer> hospitalMaps = new HashMap<String, Integer>() {
 			/**
 			 * 初始化十佳医院列表
@@ -105,6 +106,39 @@ public class DB2XesMain {
 					+ "\' AND USER_ID > 0 ORDER BY USER_ID) AS t1 LEFT JOIN (SELECT user_id AS DAOYI_ID FROM qyw.daoyi_users_20151207_07 ORDER BY DAOYI_ID) AS t2 ON t1.USER_ID = t2.DAOYI_ID WHERE t2.DAOYI_ID IS "
 					+ daoyi_query + "NULL ORDER BY VISIT_TIME";
 			stmt.executeUpdate(query);
+			long finishTime = System.currentTimeMillis(); // 获取结束时间
+			System.out.println(query + " 运行时间： " + (finishTime - beginTime)
+					/ 1000 / 60 + "min, " + (finishTime - beginTime)
+					% (1000 * 60) / 1000 + "s, " + (finishTime - beginTime)
+					% (1000 * 60) % 1000 + "ms");
+
+			// generate
+			DB2XesMain.generateXES(filepath, descname, xesname, eventprefix);
+		}
+	}
+	
+	// 生成各VISIT_OP_CAT对应的XES（按模块分析）-> VISIT_OP_CAT = SUBSTRING_INDEX(VISIT_OP,'/', 3)
+	public static void genXES_AllModule(String filepath) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		// get VISIT_OP_CAT DB
+		Connection con = DB2Xes.getCon("qyw");
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt
+				.executeQuery("SELECT DISTINCT VISIT_OP_CAT FROM qyw.visit_op_cat_20151207_07");
+		while (rs.next()) {
+			String visit_op_cat = rs.getString("VISIT_OP_CAT");
+
+			String descname = "ALL" + visit_op_cat.replaceAll("/", "_");
+			String xesname = visit_op_cat;
+			String eventprefix = "功能模块";
+
+			// prepare (produce data to db2xes.xesevents)
+			Statement stmt0 = con.createStatement();
+			Statement stmt1 = con.createStatement();
+			long beginTime = System.currentTimeMillis(); // 获取开始时间
+			stmt0.executeUpdate("TRUNCATE TABLE db2xes.xesevents");
+			String query = "INSERT INTO db2xes.xesevents SELECT CASE_ID, VISIT_TIME, USER_ID, CONCAT_WS(\'+\',VISIT_MEAN,SUBSTRING_INDEX(VISIT_OP,\'/\',\'-1\')) AS VISIT_MEAN FROM qyw.c_cus_events_20151207_07 WHERE USER_ID > 0 AND SUBSTRING_INDEX(VISIT_OP,\'/\', 3) = \'"
+					+ visit_op_cat + "\' ORDER BY VISIT_TIME";
+			stmt1.executeUpdate(query);
 			long finishTime = System.currentTimeMillis(); // 获取结束时间
 			System.out.println(query + " 运行时间： " + (finishTime - beginTime)
 					/ 1000 / 60 + "min, " + (finishTime - beginTime)
